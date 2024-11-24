77b4cc014a0554e2ccdebe96800921ac024224ef
FBasavilbaso <fedebasa05@gmail.com>
2024-11-24 18:58:02 -0300

    Instalando session

--- proyecto/app.js
+++ proyecto/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require("express-session");
+
 var indexRouter = require('./routes/index');
 const productsRouter = require("./routes/products");
 const usersRouter = require("./routes/users");
@@ -19,6 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "Sera nuestro secreto",
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', indexRouter);
 app.use("/products", productsRouter);
